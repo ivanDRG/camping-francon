7a6dc614b639e549a4642898d61bf42971b2c69b
ivanDRG <ividelrio04@gmail.com>
2020-06-02 15:11:02 +0200

    About Page files

--- src/components/AboutES.jsx
+++ src/components/AboutES.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import bigImage from "./images/homeimage2.jpg"
+import road from "./images/road.jpg"
+import bungalow from "./images/bungalow.jpg"
+import {isBrowser} from "react-device-detect"
 
 function AboutES() {
     return(
-        <div className="row relative noMargin">
-        <div className="col-sm-12 col-md-6 colAbout">
-        </div>
-        <div className="col-sm-6 col-md-3 colAbout">
-        <div>
-        <h1>Hello</h1>
-        </div>
+        <div className="row relative aboutContainer noMargin">
+        {isBrowser && <div className="col-sm-12 col-md-6 colAbout">
+        <img src={bigImage} alt="" className="bigImg" />
+        </div>}
+        <div className="col-6 col-md-2 colAbout ">
+            <div className="textAbout marginBottom">
+                <h1>Camping</h1>
+                <p>Una forma divertida y relajante de disfrutar de la playa y el sol, sin preocupaciones ni distracciones.</p>
+            </div>
             <div >
-            <h1>Hello</h1>
-        </div>
+                <img src={road} alt="" className="aboutImage"/>
+            </div>
         </div>
         
-        <div className="col-sm-6 col-md-3 colAbout">
-        <div>
-        <h1>Hello</h1>
-        </div>
-            <div >
-            <h1>Hello</h1>
-        </div>
+        <div className="col-6 col-md-2 colAbout">
+            <div>
+                <img src={bungalow} alt="" className="aboutImage"/>
+            </div>
+            <div className="textAbout marginTop">
+                <h1>Hello</h1>
+            </div>
         </div>
         
         </div>
